6b8d6695250f6f5d865eda3a693e3d6542f86be6
nwang19 <wangnicole19@gmail.com>
2018-02-26 20:36:56 -0800

    trying to make the buttons actually do something

--- app/src/main/java/com/example/nicole/testapp/MainActivity.java
+++ app/src/main/java/com/example/nicole/testapp/MainActivity.java
@@ -2,6 +2,8 @@ package com.example.nicole.testapp;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+//import android.app.*;
+import android.view.*;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +11,10 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void buttonOnClick(View v)
+    {
+
     }
 }
