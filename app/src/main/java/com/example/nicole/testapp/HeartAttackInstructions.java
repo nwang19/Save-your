b9d02bb0d47ffa05b57262bbacd68e98fd0f3b7c
nwang19 <wangnicole19@gmail.com>
2018-03-19 17:39:34 -0700

    buttons happy AND kinda work

---
+++ app/src/main/java/com/example/nicole/testapp/HeartAttackInstructions.java
@@ -0,0 +1,15 @@
+package com.example.nicole.testapp;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class HeartAttackInstructions extends AppCompatActivity {
+//but it's actually symptoms
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_heart_attack_instructions);
+    }
+
+}
--- app/src/main/java/com/example/nicole/testapp/MainActivity.java
+++ app/src/main/java/com/example/nicole/testapp/MainActivity.java
@@ -4,17 +4,31 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 //import android.app.*;
 import android.view.*;
+import android.widget.*;
+import android.graphics.Color;
 
 public class MainActivity extends AppCompatActivity {
 
+    Button myButton;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        myButton = (Button)findViewById(R.id.call911);
+        myButton.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                findViewById(R.id.textView).setBackgroundColor(Color.CYAN);
+            }
+        });
     }
 
-    public void buttonOnClick(View v)
+    public void sendMessage(View view){
+        System.out.println("button clicked");
+    }
+
+    /*public void buttonOnClick(View v)
     {
 
-    }
+    }*/
 }
