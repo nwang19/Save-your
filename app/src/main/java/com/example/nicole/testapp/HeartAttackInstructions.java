91b85100bb85b2eda1ae28fa692c2547b518adc1
Mona Lisa <ellie.cho98@gmail.com>
2018-04-01 23:19:19 -0700

    Updated chest pain

--- app/src/main/java/com/example/nicole/testapp/HeartAttackInstructions.java
+++ app/src/main/java/com/example/nicole/testapp/HeartAttackInstructions.java
@@ -1,7 +1,9 @@
 package com.example.nicole.testapp;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class HeartAttackInstructions extends AppCompatActivity {
 //but it's actually symptoms
@@ -11,5 +13,8 @@ public class HeartAttackInstructions extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_heart_attack_instructions);
     }
-
+    public void openMoreChestPain(View view){
+        Intent intent = new Intent(HeartActivityInstructions.this, MoreChestPain.class);
+        startActivity(intent);
+    }
 }
---
+++ app/src/main/java/com/example/nicole/testapp/MoreChestPain.java
@@ -0,0 +1,13 @@
+package com.example.nicole.testapp;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MoreChestPain extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_more_chest_pain);
+    }
+}
